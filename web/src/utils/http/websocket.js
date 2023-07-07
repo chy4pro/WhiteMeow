d4d89b3cb225462191922d93e31c31a00f3d79e8
james <jing2215@foxmail.com>
2023-07-07 17:48:55 +0800

    添加心跳

--- web/src/utils/http/websocket.js
+++ web/src/utils/http/websocket.js
@@ -72,8 +72,8 @@ export default class Socket {
     _heartCheck() {
         this.pingInterval = setInterval(() => {
             if(this.ws.readyState === 1) {
-                // this.ws.send(JSON.stringify({heartbeat: true}))
-                this.ws.ping()
+                this.ws.send(new Int8Array([9]))
+                // this.ws.ping()
 
             }
         },this._timeout)
