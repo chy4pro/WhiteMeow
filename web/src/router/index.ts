276173ded01db2778381fdc2e473022b032e077b
james <jing2215@foxmail.com>
2023-07-27 10:14:54 +0800

    add route

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -3,6 +3,12 @@ import home from '@/views/home/index.vue';
 import chat from '@/views/chat/index.vue';
 import feedbackSuccess from '@/views/feedbackSuccess/index.vue';
 import feedBack from '@/views/feedBack/index.vue';
+import register from '@/views/register/index.vue';
+import setPassword from '@/views/setPassword/index.vue';
+import setProfile from '@/views/setProfile/index.vue';
+import login from '@/views/login/index.vue';
+import protocol from '@/views/protocol/index.vue';
+import privacy from '@/views/privacy/index.vue';
 // import intro from '@/views/intro/index.vue';
 // import smartForm from '../components/SmartForm/index.vue';
 
@@ -26,6 +32,36 @@ export const routes: Array<RouteRecordRaw>  = [
     path: '/feedbackSuccess',
     name: 'feedbackSuccess',
     component: feedbackSuccess
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: register
+  },
+  {
+    path: '/setPassword',
+    name: 'setPassword',
+    component: setPassword
+  },
+  {
+    path: '/setProfile',
+    name: 'setProfile',
+    component: setProfile
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: login
+  },
+  {
+    path: '/protocol',
+    name: 'protocol',
+    component: protocol
+  },
+  {
+    path: '/privacy',
+    name: 'privacy',
+    component: privacy
   }
 ]
 
