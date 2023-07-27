bd4cf205e61eae5f622f27ddd0efd3f703638265
james <jing2215@foxmail.com>
2023-07-27 10:12:47 +0800

    feat:自适应

--- web/src/utils/rem.ts
+++ web/src/utils/rem.ts
@@ -2,7 +2,7 @@ const baseSize = 10; //根节点字体大小
 // 设置 rem 函数
 const setRem = ()=> {
   // 当前页面宽度相对于 750 宽的缩放比例，可根据自己需要修改。
-  const scale = document.documentElement.clientWidth / 1366;
+  const scale = document.documentElement.clientWidth / 1920;
   // 设置页面根节点字体大小
   document.documentElement.style.fontSize = baseSize * Math.min(scale, 2) + 'px';
 }
