30b3576a078984974a23638f038109ad4d46bc2f
james <jing2215@foxmail.com>
2023-08-01 19:13:50 +0800

    backup

--- web/src/views/chat/index.vue
+++ web/src/views/chat/index.vue
@@ -1,39 +1,119 @@
 <template>
 
       <!-- <div class="arrow-left" @click="router.go(-1)"></div> -->
-
-      <div class="wh-full box-border overflow-y-auto relative mt-12.2rem mb-12rem py-30px" ref="messageList">
-        <div class="more-btn" @click="showMore" v-if="recordList.page*recordList.page_size < pageTotal">查看更多</div>
-
-        <div class="flex flex-items-end mb-10px flex-nowrap px-30px " v-for="(message, index) in messages" :key="index" :class="message.isUser === true ? 'flex-justify-end' : 'flex-justify-start'">
-          <div class="
-            flex-self-start
-            color-[--green-1]
-            max-w-1/2
-            border-2
-            border-solid
-            border-[--gray-1]
-            b-rd-[15px]
-            bg-[--white-1]
-          ">
-            <div class="
-              mr-20px
-              whitespace-pre-line
-              p-10px
-              line-height-normal
-            ">{{ message.content }}</div>
-          </div>
-          <div class="
-            w-5
-            h-5
-            center-box
-          ">
-              <img :src="getEmojiUrl(message.emoji)" alt="" v-if="message.emoji!==0"/>
+  <div class="chat-panel-container">
+    <div class="chat-panel">
+      <div class="nav-part">
+        <Image name="catpaw-logo.svg" alt="" class="w-143px h-40px mt-16px"/>
+        <ul class="nav-part-tablist">
+          <li @mouseenter="hoverTabItem(index)" @mouseleave="leaveTabItem(index)" @click="clickTabItem(index)" v-for="(tabItem, index) in tablist" :key="index" :class="tabItem.status">
+            <div>
+              <Image :name="tabItemMap(tabItem)" alt="" class="w-24px h-24px inline-block vertical-middle mr-8px"/>
+              <span class="inline-block vertical-middle">{{ tabItem.label }}</span>
             </div>
+            <div v-if="tabItem.status === 'disable'">
+              <Image name="icon24_lock_on.svg" alt="" class="w-16px h-16px"/>
+            </div>
+          </li>
+          <!-- <li>
+            <div>
+              <Image name="icon24_ceshi_miao.svg" alt="" class="w-24px h-24px inline-block vertical-middle mr-8px"/>
+              <span class="inline-block vertical-middle">测试喵</span>
+            </div>
+            <div>
+              <Image name="icon24_lock_on.svg" alt="" class="w-16px h-16px"/>
+            </div>
+          </li>
+          <li>
+            <div>
+              <Image name="icon24_wode_miao.svg" alt="" class="w-24px h-24px inline-block vertical-middle mr-8px"/>
+              <span class="inline-block vertical-middle">我的喵</span>
+            </div>
+            <div>
+              <Image name="icon24_lock_on.svg" alt="" class="w-16px h-16px"/>
+            </div>
+          </li> -->
+        </ul>
+      </div>
+      <div class="main-part">
+        <div class="header">
+          <Image name="Avatar.svg" alt="" class="w-40px h-40px mr-16px"/>
+          <div class="font-600 text-16px line-height-20px">白小喵</div>
+        </div>
+        <div class="main">
+          <div class="wh-full box-border overflow-y-auto relative" ref="messageList">
+
+          <div class="text-center mt-16px cursor-pointer" @click="showMore">
+            <div class="w-24px h-24px flex-col-center mx-a">
+              <Image name="icon_more.svg" alt="" class="w-14px h-12.6px"/>
+            </div>
+            <div class="color-#666668 text-14px">查看历史聊天记录</div>
+          </div>
+
+          <div class="text-center mt-17px color-#666668 text-14px">
+            18:00
+          </div>
+
+          <div class="flex flex-items-end mb-13px flex-nowrap px-30px " v-for="(message, index) in messages" :key="index" :class="message.isUser === true ? 'flex-justify-end' : 'flex-justify-start'">
+            <div class="
+              flex-self-start
+              color-[#000c]
+              max-w-1/2
+              b-rd-[8px]
+              relative
+              "
+              :class="message.isUser === true ? 'bg-[#F5C4EA]' : 'bg-[#fff]'"
+              >
+              <div class="
+                mr-20px
+                whitespace-pre-line
+                text-16px
+                font-400
+                px-16px
+                py-8px
+                line-height-24px
+              ">{{ message.content }}</div>
+
+              <div class="absolute bottom-0" :class="message.isUser === true ? ' right--9px' : 'left--9px'" >
+                <Image name="chat-triangle-pink.svg" alt="" class="w-19px h-15px" v-if="message.isUser === true"/>
+                <Image name="chat-triangle-white.svg" alt="" class="w-19px h-15px" v-else/>
+              </div>
+            </div>
+
+            <div class="
+              w-5
+              h-5
+              center-box
+            ">
+                <img :src="getEmojiUrl(message.emoji)" alt="" v-if="message.emoji!==0"/>
+              </div>
+          </div>
+        </div>
+        </div>
+        <div class="footer">
+          <div class="bg-white h-56px rounded-8px w-full mr-30px">
+            <input
+              type="text"
+              class="wh-full outline-none border-none rounded-8px indent-24px"
+              placeholder="你想和我聊些什么？......"
+              v-model="newMessage"
+              @keyup.enter.native="sendMessage"
+            />
+          </div>
+          <div class="" @click="sendMessage" :disabled="!isConnect">
+            <Image name="icon24_send.svg" class="" />
+          </div>
         </div>
       </div>
+    </div>
+    <div class="absolute left--33.8rem top-43.2rem">
+      <Image name="animation-cat.svg" class="w-49.6rem h-38rem" />
+    </div>    
+  </div>
 
-      <div class="
+
+
+      <!-- <div class="
         w-full
         fixed
         bottom-0
@@ -55,7 +135,7 @@
           </div>
         </div>
         <Image name="111.gif" class="self-start" alt="" :width="'80px'" :height="'80px'"/>
-      </div>
+      </div> -->
 </template>
 
 <script setup lang="ts">
@@ -84,7 +164,7 @@ const isConnect = ref(false);//是否连接websocket
 const messages = ref<any>([]);
 const scrollBottomFlag = ref(false);
 const recordList = reactive({
-  page: 1,
+  page: 0,
   page_size: 10,
   user: storage.getItem('token') ? storage.getItem('newUserId') : storage.getItem('userId') || genId('userId',1),
   open_kf_id: 'wkWpQ2GQAAPtHdT-Jdk4ltXYZKlnHoSA',
@@ -92,7 +172,54 @@ const recordList = reactive({
   end_created_at: ''
 });
 
+const tablistMap = [
+  {
+    path: 'icon_ly_',
+    label: '疗愈喵',
+    status: 'press'
+  },
+  {
+    path: 'icon_cs_',
+    label: '测试喵',
+    status: 'disable'
+  },
+  {
+    path: 'icon_wd_',
+    label: '我的喵',
+    status: 'disable'
+  }
+]
+const tablist = reactive(JSON.parse(JSON.stringify(tablistMap)))
 
+const hoverTabItem = (index:number) =>{
+  let status = tablist[index].status
+  console.log(status)
+  if(status === 'disable' || status === 'press') return
+
+  tablist[index].status = 'hover'
+}
+
+const leaveTabItem = (index:number) =>{
+  let status = tablist[index].status
+  if(status === 'disable' || status === 'press') return
+  
+  const oldStatus = tablistMap[index].status
+  tablist[index].status = oldStatus
+}
+
+const clickTabItem = (index:number) => {
+  let status = tablist[index].status
+  if(status === 'disable') return
+
+  for(let i = 0; i < tablist.length; i++){
+    tablist[i].status = 'normal'
+  }
+  tablist[index].status = 'press'
+}
+
+const tabItemMap = (item:any) =>{
+  return `${item.path}${item.status}.svg`
+}
 // 发送首页传过来的第一条消息
 const sendFirstMessage = ()=>{
   if(messageStore.firstMessage){
@@ -221,10 +348,10 @@ onMounted(()=>{
     console.log('myVariable 变化了:', newValue);
     if(newValue === 'open'){
       isConnect.value = true;
-      let result = await getChatRecord();
-      if(Object.keys(result as object).length > 0){
-        sendFirstMessage();
-      }
+      // let result = await getChatRecord();
+      // if(Object.keys(result as object).length > 0){
+      //   sendFirstMessage();
+      // }
     }
     else{
       isConnect.value = false;
@@ -305,5 +432,55 @@ const getChatRecord = async() => {
   color: #fff;
   font-size: 20px;
   cursor: pointer;
+}
+.chat-panel-container{
+  --at-apply: 
+  w-108rem
+  h-72rem
+  relative
+  z-999
+  mt-48px
+}
+.chat-panel{
+  --at-apply: 
+  wh-full
+  flex-row-start
+  rounded-24px
+bg-[rgba(255,255,255,0.7)]
+  backdrop-blur-11.5px
+  overflow-hidden;
+}
+.chat-panel > .nav-part {
+  --at-apply: self-start px-16px;
+}
+.chat-panel  .nav-part-tablist {
+  --at-apply: p-0 mb-0 w-full list-none mt-32px;
+}
+.chat-panel  .nav-part-tablist > li {
+  --at-apply: rounded-6px py-16px px-8px flex-row-between cursor-pointer;
+}
+.chat-panel  .nav-part-tablist > li.press {
+  background: linear-gradient(90deg, #FCD3FF 22.92%, rgba(255, 255, 255, 0.00) 100%);
+}
+.chat-panel  .nav-part-tablist > li:hover:not(.press, .disable) {
+  background: linear-gradient(90deg, #FDE9FF 22.92%, rgba(255, 255, 255, 0.00) 100%);
+}
+.chat-panel  .nav-part-tablist > li.disable {
+  --at-apply: color-#BABABA bg-none cursor-not-allowed;
+}
+.chat-panel > .main-part {
+  --at-apply: self-start wh-full flex-col-start;
+}
+
+.chat-panel > .main-part > .header {
+  --at-apply: w-full py-12px px-24px flex-row-start bg-white;
+}
+.chat-panel > .main-part > .main {
+  --at-apply: w-full h-55.1rem bg-#f2f2f2;
+}
+.chat-panel > .main-part > .footer {
+  --at-apply: w-full h-104px bg-#F8F8F8 shadow-0px-4px-12px-0px flex-row-start px-24px;
+  /* background: #F8F8F8;
+box-shadow: 0px 4px 12px 0px rgba(0, 0, 0, 0.02); */
 }
 </style>
