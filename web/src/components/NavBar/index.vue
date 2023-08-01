1a1e7af19677bdeb56e7a7e1d24ef6c643337610
james <jing2215@foxmail.com>
2023-08-01 18:12:41 +0800

    update

--- web/src/components/NavBar/index.vue
+++ web/src/components/NavBar/index.vue
@@ -20,7 +20,7 @@
       <!-- <Image name="comment.svg" alt="" :width="'40px'" :height="'40px'" class="mr-40px cursor-pointer" @click="goFeedBack"/> -->
       <transition name="fade">
         <div v-if="open" class="absolute left-50% transform
-          translate-x--50% top-48px w-128px h-a bg-white rounded-8px overflow-hidden" style="box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.25);">
+          translate-x--50% top-4.8rem w-128px h-a bg-white rounded-8px overflow-hidden" style="box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.25);">
           <div class="flex-col-center">
             <div class="px-12px w-full">
               <div class="w-full text-center pb-12px color-#666 text-16px mt-20px border-b border-b-solid border-b-[#E7E7E7]">
