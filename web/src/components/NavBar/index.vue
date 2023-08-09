136fb1f52937ec67e662c5b926f4127a44ac7aa9
james <jing2215@foxmail.com>
2023-08-09 16:11:43 +0800

    fix:导航栏

--- web/src/components/NavBar/index.vue
+++ web/src/components/NavBar/index.vue
@@ -11,7 +11,7 @@
     bg-[rgba(255,255,255,0.3)]
       backdrop-blur-37px
     "
-    @mouseleave="open = false">
+    >
 
     <div class="ml-40px cursor-pointer w-165px h-43px" @click="goHome">
       <SvgImage name="catpaw-logo.svg" class="wh-full" />
@@ -23,27 +23,29 @@
       <SvgImage name="personal.svg" class="wh-full" />
       <!-- <Image name="comment.svg" alt="" :width="'40px'" :height="'40px'" class="mr-40px cursor-pointer" @click="goFeedBack"/> -->
       <transition name="fade">
-        <div v-if="open" class="absolute left-50% transform
-          translate-x--50% top-4.8rem w-128px h-a bg-white rounded-8px overflow-hidden" style="box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.25);">
-          <div class="flex-col-center">
-            <div class="px-12px w-full">
-              <div class="w-full text-center pb-12px color-#666 text-16px mt-20px border-b border-b-solid border-b-[#E7E7E7]">
-                <span>{{ nickname }}</span>
+        <div class="pt-8px" v-if="open"  @mouseleave="open = false">
+          <div class="absolute left-50% transform
+            translate-x--50% w-128px h-a bg-white rounded-8px overflow-hidden" style="box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.25);">
+            <div class="flex-col-center">
+              <div class="px-12px w-full">
+                <div class="w-full text-center pb-12px color-#666 text-16px mt-20px border-b border-b-solid border-b-[#E7E7E7]">
+                  <span>{{ nickname }}</span>
+                </div>
               </div>
-            </div>
 
-            <div class="px-12px w-full bg-white hover:bg-[var(--pink-01)] cursor-pointer" @click="goRegister()" v-if="!loginStore.token">
-            <div class="py-12px flex-row-center ">
-              <SvgImage name="icon24_account.svg" class="w-24px h-24px" />
-              <span class="text-16px font-500 line-height-normal">注册/登录</span>
-            </div>
-            </div>
-            
-            <div class="px-12px w-full bg-white hover:bg-[var(--pink-01)] cursor-pointer" v-if="loginStore.token" @click="startLogout">
-            <div class="py-12px flex-row-center">
-              <SvgImage name="icon24_sign_out.svg"  class="w-24px h-24px"  />
-              <span class="text-16px font-500 line-height-normal">退出登录</span>
-            </div>
+              <div class="px-12px w-full bg-white hover:bg-[var(--pink-01)] cursor-pointer" @click="goRegister()" v-if="!loginStore.token">
+              <div class="py-12px flex-row-center ">
+                <SvgImage name="icon24_account.svg" class="w-24px h-24px" />
+                <span class="text-16px font-500 line-height-normal">注册/登录</span>
+              </div>
+              </div>
+              
+              <div class="px-12px w-full bg-white hover:bg-[var(--pink-01)] cursor-pointer" v-if="loginStore.token" @click="startLogout">
+              <div class="py-12px flex-row-center">
+                <SvgImage name="icon24_sign_out.svg"  class="w-24px h-24px"  />
+                <span class="text-16px font-500 line-height-normal">退出登录</span>
+              </div>
+              </div>
             </div>
           </div>
         </div>
@@ -69,7 +71,7 @@ const props = defineProps({
     default: false
   }
 });
-const open = ref(false)
+let open = ref(false)
 interface UserInfo {
   nickname: string
 }
@@ -150,6 +152,10 @@ const startLogout = async() => {
     // loader hide
     // closeToast();
   }
+}
+
+const leaveNav = () => {
+  setTimeout(()=>{open.value = false},1000)
 }
 </script>
 
