1066393824b9fa46ddd80991798da611da07a0a0
james <jing2215@foxmail.com>
2023-08-16 18:17:07 +0800

    tab map

---
+++ web/src/views/chat/tablistMap.ts
@@ -0,0 +1,20 @@
+export const tablistMap = [
+  {
+    iconName: 'icon_ly_',
+    path: '/chat/freeChat',
+    label: '聊愈喵',
+    status: 'press'
+  },
+  {
+    iconName: 'icon_cs_',
+    path: '/chat/testChat',
+    label: '测试喵',
+    status: 'disable'
+  },
+  {
+    iconName: 'icon_wd_',
+    path: '/chat/myCat',
+    label: '我的喵',
+    status: 'disable'
+  }
+]
