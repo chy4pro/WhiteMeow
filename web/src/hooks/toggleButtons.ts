194a23e5f683adea9a20a51ffe95f7650ba13139
james <jing2215@foxmail.com>
2023-08-25 17:52:48 +0800

    feat:buttons toggle逻辑

---
+++ web/src/hooks/toggleButtons.ts
@@ -0,0 +1,26 @@
+// toggleButtons.ts
+import {ref} from 'vue'
+import {useToggle} from '@vueuse/core'
+
+export function useToggleButtons(btns: string[]) {
+  const current = ref('')
+  const buttons = btns.map(button => {
+    const toggle = useToggle(button === current.value) as any
+    
+    return {
+      value: button,
+      isActive: toggle.value,
+      toggle: toggle.toggle
+    }
+  })
+
+  function handleButtonClick(value: string) {
+    current.value = value
+  }
+
+  return {
+    current,
+    buttons,
+    handleButtonClick
+  }
+}
