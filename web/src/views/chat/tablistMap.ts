af9717c6953decc87937e89229ae9320502dae75
james <jing2215@foxmail.com>
2023-08-28 16:24:01 +0800

    feat:添加tabMap

--- web/src/views/chat/tablistMap.ts
+++ web/src/views/chat/tablistMap.ts
@@ -12,9 +12,42 @@ export const tablistMap = [
     status: 'disable'
   },
   {
+    iconName: 'icon_mx_',
+    path: '/chat/game',
+    label: '冒险喵',
+    status: 'disable'
+  },
+  {
     iconName: 'icon_wd_',
     path: '/chat/myCat',
     label: '我的喵',
     status: 'disable'
+  }
+]
+
+export const loginTablistMap = [
+  {
+    iconName: 'icon_ly_',
+    path: '/chat/freeChat',
+    label: '聊愈喵',
+    status: 'press'
+  },
+  {
+    iconName: 'icon_cs_',
+    path: '/chat/testChat',
+    label: '测试喵',
+    status: 'normal'
+  },
+  {
+    iconName: 'icon_mx_',
+    path: '/chat/game',
+    label: '冒险喵',
+    status: 'normal'
+  },
+  {
+    iconName: 'icon_wd_',
+    path: '/chat/myCat',
+    label: '我的喵',
+    status: 'normal'
   }
 ]
