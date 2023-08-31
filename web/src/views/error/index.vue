a2f42bb27d8858866d7a60be8d2c490febadd63e
takal <972322652@qq.com>
2023-08-31 18:06:13 +0800

    fix path

--- web/src/views/error/index.vue
+++ web/src/views/error/index.vue
@@ -6,7 +6,7 @@ import { useRouter } from "vue-router";
 const router = useRouter();
 
 const handleLinkHome = () => {
-  router.push({ path: "chat" });
+  router.push({ path: "/chat" });
 };
 </script>
 
