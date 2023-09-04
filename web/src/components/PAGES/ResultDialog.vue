a457fbb6f3313d3984d6123859e0584ca4e1d8ad
james <jing2215@foxmail.com>
2023-09-04 14:23:32 +0800

    fix:对话框组件尺寸调整

--- web/src/components/PAGES/ResultDialog.vue
+++ web/src/components/PAGES/ResultDialog.vue
@@ -1,53 +1,56 @@
 <template>
   <div class="wh-full box-border overflow-y-auto relative bg-[url(@/assets/images/tarot-bg.png)]">
-    <div class="
+    <div class="px-6.4rem">
+      <div class="
         w-full
-        min-h-45rem
+        h-46rem
         relative
         mb-5.4rem
         mt-12.1rem
         bg-[url(@/assets/images/dialog_box_large.svg)]
         bg-no-repeat
         bg-bottom"
-        style="background-size: 100% 100%;"
+        style="background-size: 100%"
         >
-        <SvgImage name="cat_red3.svg" class="
+        <Image name="cat_red3.png" class="
           w-full
-          h-12.1rem
+          h-14rem
           absolute
-          left-0
-          top--9.5rem
-        "></SvgImage>
+          left-0rem
+          top--11.2rem
+        "></Image>
         <div class="
         h-35rem
-        absolute
-        bottom-2rem
-        overflow-auto
-        pl-9.8rem
-        pr-9.9rem
-        text-2rem
-        font-500
-        color-black
-        z-666
-        whitespace-pre-line
-        line-height-normal">{{ props.message }}</div>
-    </div>
+          absolute
+          bottom-34px
+          overflow-y-scroll
+          pl-1.6rem
+          pr-2.5rem
+          mr-2rem
+          text-2rem
+          font-500
+          color-black
+          z-666
+          whitespace-pre-line
+          line-height-normal">{{ props.message }}</div>
+      </div>
     
-    <div class="flex-col-center">
-      <a-button class="
-        w-14.9rem
-        h-6.4rem
-        flex-col-center
-        text-1.9rem
-        font-800
-        bg-[var(--text-01)]
-        cursor-pointer
-        color-white
-        rounded-6px
-        border-none
-        "
-        @click="$router.push('/chat/testChat')"
-        >更多测试</a-button>
+      <div class="flex-col-center">
+        <a-button class="
+          w-14.9rem
+          h-6.4rem
+          flex-col-center
+          text-1.9rem
+          font-800
+          bg-[var(--text-01)]
+          cursor-pointer
+          color-white
+          rounded-6px
+          border-none
+          "
+          @click="$router.push('/chat/testChat')"
+          >更多测试</a-button>
+      </div>
     </div>
   </div>
 </template>
